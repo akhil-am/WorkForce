6cd4efbfa3d6e6c483429289fc2fdf30a515d67a
akhil <akhil.am@icloud.com>
2017-01-11 23:13:02 +0530

    drawer+home

--- app/src/main/java/com/example/akhil/workforce/worker/WorkerHome.java
+++ app/src/main/java/com/example/akhil/workforce/worker/WorkerHome.java
@@ -9,12 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.akhil.workforce.R;
 public class WorkerHome extends Fragment{
-    View view;Button accept, reject;
-//DrawerLayout navigationView=(DrawerLayout)
+    View view;
+    Button accept, reject;
+    TextView jobAlert;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,18 +28,21 @@ public class WorkerHome extends Fragment{
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        jobAlert=(TextView)view.findViewById(R.id.job_alert);
         accept=(Button)view.findViewById(R.id.accept);
         reject=(Button)view.findViewById(R.id.reject);
         accept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Alert for confirmation
                 AlertDialog.Builder dialog=new AlertDialog.Builder(getActivity());
                 dialog.setTitle("CONFIRM ACTION");
                 dialog.setCancelable(true);
                 dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getContext(),"Yes",Toast.LENGTH_SHORT).show();
+                    jobAlert.setText("You have curently dont have active jobs");
+                        Toast.makeText(getContext(),"You have assigned a new job..!",Toast.LENGTH_LONG).show();
                     }
                 });
                 dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
